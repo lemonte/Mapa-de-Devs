a53d950c149f1748d9ca694868091d1055875aad
Geanderson <48225849+DevGeanderson@users.noreply.github.com>
2020-03-07 17:01:38 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-mongoose.connect("mongodb://Geanderson:lemonte@cluster0-shard-00-00-k07t8.gcp.mongodb.net:27017,cluster0-shard-00-01-k07t8.gcp.mongodb.net:27017,cluster0-shard-00-02-k07t8.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", {
+mongoose.connect("mongodb:// <key>, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
